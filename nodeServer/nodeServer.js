052a0dd0ea0c63f54103457931e9e367ebb5d565
David Sjunnesson <david@tellart.com>
2013-02-22 11:51:21 -0500

    First commit

---
+++ nodeServer/nodeServer.js
@@ -0,0 +1,48 @@
+// var app = require('http').createServer(handler)
+//   , io = require('socket.io').listen(app)
+//   , fs = require('fs')
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+  io.set('transports', ['xhr-polling']);
+
+app.listen(6789);
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  console.log("New connection");
+
+
+  socket.on('moveToNextSlide', function (data) {
+    socket.broadcast.emit('nextSlide', { slideNr: data.slideNr});
+  });  
+
+  socket.on('moveToPreviousSlide', function (data) {
+    socket.broadcast.emit('previousSlide', { slideNr: data.slideNr});
+  });
+
+  socket.on('resetSlideshow', function (data) {
+    socket.broadcast.emit('resetSlideshow',data);
+  });
+
+  socket.on('jumpToSlide', function (data) {
+    socket.broadcast.emit('jumpToSlide',data);
+  });
+
+
+
+});
